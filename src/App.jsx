4e1263802ac1a300200fd436126cb5c1edc3bd90
Vinícius Yoda <vyoda4604@gmail.com>
2023-07-17 13:21:46 -0300

    vite + react

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { RouterProvider } from "react-router-dom"
 import { Estilos } from "./componentes/EstilosGlobais/Estilos"
 import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema"
 import { router } from "./router/router"
+import { SessaoUsuarioProvider } from "./contexto/SessaoUsuario"
 
 function App () {
   return (<ProvedorTema>
     <Estilos />
     <SessaoUsuarioProvider>
-
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </SessaoUsuarioProvider>
   </ProvedorTema>)
 } 
